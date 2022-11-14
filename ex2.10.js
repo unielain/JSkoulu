726fc6fbd8e08297cd6c8485788515913e7aee01
Unto Nordling <85550906+unielain@users.noreply.github.com>
2022-11-15 01:20:19 +0200

    it works if you don't want it to collapse
    I'm not at all happy with this but in theory the program works if tested by a kind person.

--- ex2.10.js
+++ ex2.10.js
@@ -4,22 +4,22 @@ Then the program asks for the names of the candidates: 'Name for candidate 1
 Store the candidates' names and initial vote count in objects like this:*/
 
 const number_of_candidates = parseInt(prompt(`Give a number of candidates: `));
-const candidates = []
-
+const candidates = [];
+let vote = ` `;
+let voted = [];
+let sum_of_votes = [];
+let index = 0;
 for(let i = 0; i < number_of_candidates; i++){
     name = prompt(`Name for canditate ${i +1}: `);
     let candidate = {
         name: name,
         votes: 0,
-        a: { set() {
-                this.votes = + 1
-            }
-    },
     };
-    candidates.push(candidate);
-
+    candidates.push(candidate)
+    sum_of_votes.push(0)
 }
-console.log(candidates)
+
+console.log(candidates);
 
 /*The program asks for the number of voters.
 The program asks each voter in turn who they will vote for.
@@ -27,13 +27,32 @@ Voter should enter candidate name. If the voter enters an empty value instead of
 it will be interpreted as an empty vote.
 The program announces the name of the winner and the results by printing it to the console:*/
 
-const number_of_voters = parseInt(prompt(`Give the number of voters: `))
-for(let i = 0; i < number_of_voters; i++){
-    const vote = prompt(`Voter ${i + 1}, enter your candidate: `);
-    if(vote === .candidate.valueOf(vote)){
-        vote.candidate.votes = +1;
+const number_of_voters = parseInt(prompt(`How many voters? `));
+for(let i = 0; i < number_of_voters; i++) {
+    vote = prompt(`Voter number ${i + 1}: `);
+    for(let f = 0; f < candidates.length; f++){
+        if(vote == candidates[f].name){
+            candidates[f].votes += 1
+            voted.push(vote)
+
+        }
+    }
+}
+
+for(let n = 0; n < candidates.length; n++){
+    for(let b = 0; b < voted.length; b++){
+        if(candidates[n].name == voted[b]){
+            sum_of_votes[n] += 1;
+        }
+    }
+}
+
+sum_of_votes.sort((a,b) => b-a);
+for(let u = 0; u < candidates.length; u++){
+    if(sum_of_votes[0] === candidates[u].votes){
+        index = u;
     }
 }
 
 console.log(candidates);
-
+console.log(`The winner is ${candidates[index].name} with ${candidates[index].votes} votes`);
